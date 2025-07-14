01b7d300473fef2360751e170463ca2d256d7015
iyuspandey <iyuspandey@gmail.com>
2025-07-14 15:56:30 +0530

    discussion version2.0

---
+++ project-frontend/src/components/formattimeago.jsx
@@ -0,0 +1,8 @@
+export default function formatTimeAgo(dateStr) {
+    const diff = Math.floor((Date.now() - new Date(dateStr)) / 1000);
+    if (diff < 60) return `${diff}s ago`;
+    if (diff < 3600) return `${Math.floor(diff / 60)}m ago`;
+    if (diff < 86400) return `${Math.floor(diff / 3600)}h ago`;
+    return new Date(dateStr).toLocaleString();
+  }
+  
